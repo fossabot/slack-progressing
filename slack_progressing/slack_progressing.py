e32db8d3b8b871cb613db2e57e3183274524cc30
Shunta Komatsu <skmatz217@gmail.com>
2019-10-07 23:55:45 +0900

    ✨ :  Implement __call__ method

--- slack_progressing/slack_progressing.py
+++ slack_progressing/slack_progressing.py
@@ -16,13 +16,8 @@ class SlackProgressing:
         self._progress_bar = ProgressBar(length=length)
         self._slack = Slack(token=token, channel=channel)
 
-    def set_params(self, params):
-        """Set additional parameters to progress bar.
-
-        Args:
-            params (dict): Additional parameters
-        """
-        self._progress_bar.params = params
+    def __call__(self, iterable):
+        return self.progress(iterable)
 
     def progress(self, iterable):
         """Control iterations.
@@ -40,3 +35,11 @@ class SlackProgressing:
                 self._slack.post(self._progress_bar)
             else:
                 self._slack.update(self._progress_bar)
+
+    def set_params(self, params):
+        """Set additional parameters to progress bar.
+
+        Args:
+            params (dict): Additional parameters
+        """
+        self._progress_bar.params = params
